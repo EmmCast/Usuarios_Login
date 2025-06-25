360cbdabaac9d87913286e0aaf110c960ca41e05
Emmanuel <emmcast91@gmail.com>
2025-06-25 16:26:20 -0600

    inicia proyecto usuarios_login que servira para registrar usuarios y para obtener los tokens de acceso todo hecho por spring

---
+++ src/main/java/com/proyect/System_userAndLogin/SystemUserAndLoginApplication.java
@@ -0,0 +1,13 @@
+package com.proyect.System_userAndLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemUserAndLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemUserAndLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyect/System_userAndLogin/SystemUserAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.System_userAndLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemUserAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
